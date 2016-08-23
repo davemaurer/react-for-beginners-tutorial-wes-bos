2354511b276a584f04d408b970bbd9fe83b23f7b
David Maurer <davemaurermail@gmail.com>
2016-08-23 17:59:36 -0600

    Added code to use HTML5 local storage for user order state.
    Using componentWillUpdate to store order items and adding to
    componentDidMount to retrieve data and display it.

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -31,6 +31,17 @@ var App = React.createClass({
       context : this,
       state : 'fishes'
     });
+
+    var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId);
+
+    if(localStorageRef) {
+      this.setState({
+        order : JSON.parse(localStorageRef)
+      });
+    }
+  },
+  componentWillUpdate : function(nextProps, nextState) {
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
   },
   addToOrder : function(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1;
@@ -167,7 +178,7 @@ var Order = React.createClass({
     }
 
     return (
-      <li>
+      <li key={key}>
         <span>{count}</span>
         <span>lbs</span>
         {fish.name}
