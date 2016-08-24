7817798990eea9937f380b013d9765190b8351a3
David Maurer <davemaurermail@gmail.com>
2016-08-24 13:33:02 -0600

    add stylus and require CSSTransitionGroup for CSS transpiling and methods.
    Also adjust routes variable to be more in line with current video. Some translation
    lost between updates since the course was produced.

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var CSSTransitionGroup = require('react-addons-css-transition-group');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
@@ -191,18 +192,23 @@ var Order = React.createClass({
   renderOrder : function(key) {
     var fish = this.props.fishes[key];
     var count = this.props.order[key];
-    var removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}>&times</button>;
+    var removeButton = <button onClick={this.props.removeFromOrder.bind(null, key)}>&times;</button>
     if(!fish) {
       return <li key={key}>Sorry, fish no longer available! {removeButton}</li>
     }
 
     return (
       <li key={key}>
-        <span>{count}</span>
-        <span>lbs</span>
-        {fish.name}
+        <span>
+          <CSSTransitionGroup component="span" transitionName="count"
+                              transitionEnterTimeout={250}
+                              transitionLeaveTimeout={250}>
+
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>
+          lbs {fish.name} {removeButton}
+        </span>
         <span className="price">{h.formatPrice(count * fish.price)}</span>
-        {removeButton}
       </li>)
   },
   render : function() {
@@ -222,13 +228,21 @@ var Order = React.createClass({
     return (
       <div className="order-wrap">
         <h2 className="order-title">Your Order</h2>
-        <ul className="order">
+
+        <CSSTransitionGroup
+            className="order"
+            component="ul"
+            transitionName="order"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+          
           {orderIds.map(this.renderOrder)}
           <li className="total">
-          <strong>Total:</strong>
-          {h.formatPrice(total)}
+            <strong>Total:</strong>
+            {h.formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
+
       </div>
     )
   }
@@ -314,9 +328,9 @@ Routes
 
 var routes = (
   <Router history={createBrowserHistory()}>
-    <Router path="/" component={StorePicker}/>
-    <Router path="/store/:storeId" component={App}/>
-    <Router path="*" component={NotFound}/>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+    <Route path="*" component={NotFound}/>
   </Router>
 );
 
